85355a12c57aa0ef95d71cbf1879f2099a90d1c8
shobhna1 <shobhnasinha@yahoo.com>
2021-09-06 18:56:51 -0700

    added users controller, signup form and routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
   resources :articles
+  get 'signup', to: 'users#new'
+  post 'users', to: 'users#create'
+  resources :users, except: [:new]
   #resources :articles, only: [:show, :index, :new, :create, :edit, :update, :destroy]
 end
