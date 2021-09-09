0e2d167d3d9d6aa1e283c694d045f9504b5b0533
shobhna1 <shobhnasinha@yahoo.com>
2021-09-08 21:22:58 -0700

    add restrict actions in controllers on the backend

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
   before_action :set_article, only: [:show, :edit, :update, :destroy]
+  before_action :require_user, except: [:show, :index]
+  before_action :require_same_user, only: [:edit, :update, :destroy]
 
   def show
   end
@@ -52,5 +54,12 @@ class ArticlesController < ApplicationController
   
   def article_params
     params.require(:article).permit(:title, :description)
+  end
+  
+  def require_same_user
+    if current_user != @article.user
+      flash[:alert] = "Only allowed to edit or delete your own articles"
+      redirect_to @article
+    end
   end
 end
